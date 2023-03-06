80c5ddeb860259fc5e89b5b76ba32cb4696a1f29
sebaandino <elias_andino@hotmail.com>
2023-03-06 16:05:23 -0300

    prueba de bucles

---
+++ clase1/src/clase1/bucles_puebas.java
@@ -0,0 +1,19 @@
+package clase1;
+
+public class bucles_puebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10 , a= 20;
+		System.out.println("contador:");
+		while(n < a) {
+			n = n + 1;
+			System.out.println(n);
+		}
+		System.out.println("bucle 2 en 2:");
+				for (int i=0;i<=20;i = i + 2) {
+					System.out.println(i);
+				}
+				}
+
+	}
